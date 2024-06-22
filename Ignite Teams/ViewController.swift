3bda786d86395d205a753f39fb35fcf2e605154a
Yuri Corrêa <yuricorrea@ioasys.com.br>
2024-06-21 21:37:23 -0300

    Initial Commit

---
+++ Ignite Teams/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ignite Teams
+//
+//  Created by Yuri Correa on 21/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
